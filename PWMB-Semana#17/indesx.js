52eac66c7eeac991d3b5b9b5ba705e4cb248fe2c
stefanydnf <126208778+stefanydnf@users.noreply.github.com>
2023-12-08 02:47:43 -0300

    Add files via upload

---
+++ PWMB-Semana#17/indesx.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+console.log (express);
+
+const express = require('express;')
+
+const server = express ();
+
+localhost:80000/pedido
+server.get('/pedido', () => 
+{ console.log("Acessou a Rota")
+});
+server.listen(8000);
+
